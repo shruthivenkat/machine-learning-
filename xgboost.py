17b430717b88734f83207c1e2bc9a0f7dc5dc5d7
shruthivenkat <amritashruthi@gmail.com>
2018-03-08 07:12:20 +0530

    Update xgboost.py

--- xgboost.py
+++ xgboost.py
@@ -7,7 +7,9 @@ Created on Sun Feb 11 21:08:34 2018
 
 from numpy import loadtxt
 from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix
 from sklearn import model_selection
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas 
@@ -22,11 +24,26 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, Y, test_s
 
 model = XGBClassifier()
 model.fit(X_train, y_train)
-print(model)
+#print(model)
 
 y_pred = model.predict(X_test)
 predictions = [value for value in y_pred]
 
+#cross validation
+
+scoring='accuracy'
+results=[]
+kfold=model_selection.KFold(n_splits=10,random_state=seed)
+cv_results=model_selection.cross_val_score(model,X_train,y_train,cv=kfold,scoring=scoring)
+results.append(cv_results)
+msg="XGBOOST cross-validation: %f (%f)"%(cv_results.mean(),cv_results.std())
+print(msg)
 
 accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+print("xgboost Accuracy: %.2f%%" % (accuracy * 100.0))
+
+print("confusion matrix:")
+print(confusion_matrix(y_test,predictions))
+
+print("classification summary report:")
+print(classification_report(y_test,predictions))
